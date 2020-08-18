44f9fa4d7d70bf52c1bf19e7c5b37da8b8b0b268
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2020-08-18 10:16:23 -0300

    Theme support updated

--- src/resources/theme.js
+++ src/resources/theme.js
@@ -6,20 +6,31 @@ import {
     ReferenceInput, 
     AutocompleteInput, 
     NumberInput,
+    FormDataConsumer,
     regex
 
 } from 'react-admin';
 
+import { ColorInput } from 'react-admin-color-input'
 
-const validateColor =  regex(/^#\d{6}$/, 'Must be a color Code, example: #11cc01')
+//const validateColor =  regex(/^#\d{6}$/, 'Must be a color Code, example: #11cc01')
 const validateLogoUrl = regex(/^https:\/\/.*\..*$/, 'Must be be a valid https URL')
 
 export const ThemeCreate = ({ permissions, ...props }) => (
     <Create {...props}>
         <SimpleForm>
             <TextInput source="logoUrl" validate={ validateLogoUrl }/>
-            <TextInput source="mainColor" validate={ validateColor } />
             <NumberInput source="logoWidth" defaultValue={ 60 } />
+            <ColorInput source="mainColor"/>
+            <FormDataConsumer>
+                {({ formData }) =>
+                     <img alt=""
+                         src={ formData.logoUrl }
+                         width={ formData.logoWidth }
+                         style={{background: formData.mainColor}}
+                     />
+                }
+            </FormDataConsumer>
             <ReferenceInput source="ctx_id" reference="contexts" >
                 <AutocompleteInput optionText="name" />
             </ReferenceInput>
