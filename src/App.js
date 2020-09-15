b29358462d7acab0b07ebc1a9bf2be402a57ed0e
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2020-09-15 11:41:34 -0300

    Added i18n support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Admin, Resource, resolveBrowserLocale} from 'react-admin';
+import { Admin, Resource} from 'react-admin';
 // Rest Providers
 import dataProvider from './rest/dataProvider';
 import authProvider from './rest/authProvider';
@@ -11,27 +11,12 @@ import { ContextList, ContextCreate, ContextEdit } from './resources/context'
 import { MailboxList, MailboxCreate, MailboxEdit } from './resources/mailbox'
 
 //Theming
-import { Dashboard } from './dashboard/dashboard'
 import { Face, Domain,  AccountTree, Mail, Store} from '@material-ui/icons';
+import { Dashboard } from './dashboard/dashboard'
 import { ThemeCreate } from './resources/theme';
 
 
-//Translating
-import polyglotI18nProvider from 'ra-i18n-polyglot';
-import englishMessages from 'ra-language-english';
-import ptBrMessages from 'ra-language-pt-br';
-
-const messages = {
-  en: englishMessages,
-  pt: ptBrMessages
-};
-
-
-
-const i18nProvider = polyglotI18nProvider(
-  locale => messages[locale] ? messages[locale] : messages.en,
-  resolveBrowserLocale()
-);
+import i18nProvider from './translation/i18nProvider'
 
 
 const App = () => (
--- src/dashboard/welcome.js
+++ src/dashboard/welcome.js
@@ -4,20 +4,24 @@ import { Home, Code } from '@material-ui/icons';
 import { Title } from 'react-admin';
 import Typography from '@material-ui/core/Typography';
 
+import { useTranslate } from 'react-admin';
+
+
 var config = require('../config.json');
 
 const Welcome = () => {
+    const translate = useTranslate();
     return (
         <Card variant="outlined">
-            <Title title="Welcome Open-Xchange Administration Panel" />
+            <Title title="Open-Xchange Administration Panel" />
             <CardContent>           
                 <Typography gutterBottom variant="h5" component="h2">
-                    Welcome to OX Administration Panel
+                    {translate('general.welcome')}
                 </Typography>
                 <CardActions>
                     <Button href={ config.WEB_URL + "/appsuite"}>
                             <Home style={{ paddingRight: '0.5em' }} />
-                            Access Webmail
+                            {translate('general.access_webmail')}
                     </Button>
                     <Button href={ config.API_URL }>
                             <Code style={{ paddingRight: '0.5em' }} />
